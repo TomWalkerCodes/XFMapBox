a2b79b46b5b9619b8c932cbc3e2ff43d33ab7111
Tom Walker <Tom Walker@TOMSDEVSURFACE>
2017-05-01 10:17:16 -0600

    Add project files.

---
+++ XFMapBox/XFMapBox.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Mapbox;
+
+namespace XFMapBox.Droid
+{
+    [Activity(Label = "XFMapBox", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            MapboxAccountManager.Start(this, "pk.eyJ1IjoiZmNjbWFuYWdlbWVudHNvZnR3YXJlIiwiYSI6ImNpb3BzMnR6ODAwNm50d2x6eWl4dTJsM3oifQ.NG-ngOgssSz0vL-yiFIRew");
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ XFMapBox/XFMapBox.Android/MapBoxRenderer.cs
@@ -0,0 +1,65 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XFMapBox.Droid;
+using XFMapBox.Controls;
+using Mapbox.Geometry;
+using Mapbox.Maps;
+using Mapbox.Annotations;
+using Mapbox.Camera;
+
+[assembly: ExportRenderer(typeof(FCCMapBox), typeof(MapBoxRenderer_Droid))]
+namespace XFMapBox.Droid
+{
+    public class MapBoxRenderer_Droid : ViewRenderer<FCCMapBox, MapView>, IOnMapReadyCallback
+    {
+        private static readonly LatLng LOS_ANGELES = new LatLng(34.0539440, -118.242622);
+        private static readonly LatLng NEW_YORK = new LatLng(40.712730, -74.005953);
+
+        private MapView _MapView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<FCCMapBox> e)
+        {
+            base.OnElementChanged(e);
+
+            MapboxMapOptions options = new MapboxMapOptions();
+            options.InvokeScrollGesturesEnabled(true);
+            options.InvokeRotateGesturesEnabled(true);
+            options.InvokeTiltGesturesEnabled(true);
+            options.InvokeZoomGesturesEnabled(true);
+
+            _MapView = new MapView(Context, options);
+            _MapView.OnCreate(null);
+            _MapView.SetStyle(Mapbox.Constants.Style.MapboxStreets);
+            _MapView.GetMapAsync();
+            SetNativeControl(_MapView);
+        }
+
+        public void OnMapReady(MapboxMap map)
+        {
+            var uiSettings = map.UiSettings;
+            uiSettings.SetAllGesturesEnabled(true);
+
+            var marker = new MarkerOptions();
+
+            marker.SetTitle("Los Angeles");
+            marker.SetSnippet("City Hall");
+            marker.SetPosition(LOS_ANGELES);
+
+            map.AddMarker(marker);
+
+            marker.SetTitle("New York");
+            marker.SetSnippet("City Hall");
+            marker.SetPosition(NEW_YORK);
+
+            map.AddMarker(marker);
+
+            var bounds = new LatLngBounds.Builder()
+                .Include(NEW_YORK)
+                .Include(LOS_ANGELES)
+                .Build();
+
+            map.MoveCamera(CameraUpdateFactory.NewLatLngBounds(bounds, 8));
+
+        }
+    }
+}
